52127c9fe035c209a28fa96a67075be54226ddd5
Rana Dawar Abdullah <imdawar090@gmail.com>
2024-10-17 20:01:20 +0500

    Observable

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,32 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { Observable } from "rxjs";
 
 const index = () => {
+  const notError = false;
+  const numberObservable = new Observable((subscriber) => {
+    if (notError) {
+      subscriber.next(1);
+      subscriber.next(2);
+      subscriber.next(3);
+      subscriber.complete();
+    } else {
+      subscriber.error("An error occurred");
+    }
+  });
+
+  // Subscribe to the observable
+  numberObservable.subscribe({
+    next: (value) => console.log(`Received value: ${value}`),
+    error: (error) => console.log(`Error: ${error}`),
+    complete: () => console.log("Observable completed"),
+  });
   return (
-    <SafeAreaView>
+    <SafeAreaView style={{ flex: 1 }}>
       <Text>index</Text>
     </SafeAreaView>
   );
 };
 
 export default index;
-
-const styles = StyleSheet.create({});
